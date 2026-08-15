b0bf110634cb07a5de787ea21f796b27fe635ed6
Rishi Vora <rishi.get219@gmail.com>
2026-08-16 04:15:16 +0530

    Terraform CI ready
    * Pin Terraform version to 1.14.6
    
    * Add Azure authentication to Terraform CI
    
    * chnaging cred for  Terraform CI
    
    * chnaging RG
    
    * added backend block in provider
    
    * added backend block

--- terraform/projects/azure-resource-group/provider.tf
+++ terraform/projects/azure-resource-group/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.81.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-backend2"
+    storage_account_name = "storagebackend2" # Must be globally unique
+    container_name       = "tfstatebackend2"
+    key                  = "backend.terraform.tfstate" # Name of your state file
+  }
 }
 
 provider "azurerm" {
