d39e20f17e4e17fd328b3b0800652dbbb73fbf7b
rishiget219 <rishi.get219@gmail.com>
2026-08-16 03:12:33 +0530

    added backend block in provider

--- terraform/projects/azure-resource-group/provider.tf
+++ terraform/projects/azure-resource-group/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.81.0"
     }
   }
+
+    backend "azurerm" {
+    resource_group_name  = "rg-rishi"
+    storage_account_name = "storeagepipeline"      # Must be globally unique
+    container_name       = "tfstate-pipeline"
+    key                  = "pipeline.terraform.tfstate" # Name of your state file
+  }
 }
 
 provider "azurerm" {
